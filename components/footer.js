c175e32a3b4512203441977634a3a85970ff2d12
shahreukh <shahreukh@gmail.com>
2023-02-25 20:56:09 +0300

    completed

---
+++ components/footer.js
@@ -0,0 +1,31 @@
+import { ImGithub, ImTwitter, ImLinkedin } from "react-icons/im";
+import Link from 'next/link'
+import Newsletter from "./_child/newsletter";
+
+export default function footer() {
+
+  const bg = {
+    backgroundImage : "url('/images/footer.png')",
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: "bottom left"
+  }
+
+  return (
+    <footer className="bg-gray-50" style={bg}>
+      <Newsletter></Newsletter>
+      <div className="container mx-auto flex justify-center py-12">
+          <div className="py-5">
+              <div className="flex gap-6 justify-center">
+                    <Link href={"/"}><a><ImGithub color="#888888" /></a></Link>
+                    <Link href={"/"}><a><ImTwitter color="#888888" /></a></Link>                    
+                    <Link href={"/"}><a><ImLinkedin color="#888888" /></a></Link>
+              </div>
+
+              <p className="py-5 text-gray-400">Copyright ©2023 All rights reserved | This template is made by Shahrukh Mirza</p>
+              <p className="text-gray-400 text-center">Terms & Condition</p>
+          </div>
+      </div>
+
+    </footer>
+  )
+}
---
+++ pages/api/data.js
@@ -0,0 +1,349 @@
+export default {  
+    Trending : [
+        {
+            id: 1,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/img1.jpg",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+            `,
+            published: "Jan 13,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 2,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/img2.jpg",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+            `,
+            published: "may 4,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 3,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/img3.png",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+            `,
+            published: "Sep 22,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 4,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/img4.png",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+            `,
+            published: "Feb 11,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 5,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/img5.png",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+            `,
+            published: "Jun 14,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        }
+    ],
+    Posts : [
+        {
+            id: 1,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/posts/img1.jpg",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, qui ea assumenda illo quidem 
+            consequatur ducimus cum aliquam est minus provident placeat, nesciunt vitae. 
+            Et aperiam magni non quasi est asperiores nobis atque suscipit incidunt dignissimos! Harum et sunt ea.
+
+            When she reached the first hills of the Italic Mountains, she had a last view back on the skyline 
+            of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, 
+            the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.
+
+            Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.
+            Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+
+            A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+            It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+            `,
+            published: "Jun 3,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 2,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/posts/img2.png",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, qui ea assumenda illo quidem 
+            consequatur ducimus cum aliquam est minus provident placeat, nesciunt vitae. 
+            Et aperiam magni non quasi est asperiores nobis atque suscipit incidunt dignissimos! Harum et sunt ea.
+
+            When she reached the first hills of the Italic Mountains, she had a last view back on the skyline 
+            of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, 
+            the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.
+
+            Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.
+            Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+
+            A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+            It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+            `,
+            published: "Jun 5,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 3,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/posts/img3.png",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, qui ea assumenda illo quidem 
+            consequatur ducimus cum aliquam est minus provident placeat, nesciunt vitae. 
+            Et aperiam magni non quasi est asperiores nobis atque suscipit incidunt dignissimos! Harum et sunt ea.
+
+            When she reached the first hills of the Italic Mountains, she had a last view back on the skyline 
+            of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, 
+            the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.
+
+            Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.
+            Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+
+            A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+            It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+            `,
+            published: "Jun 7,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 4,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/posts/img4.png",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, qui ea assumenda illo quidem 
+            consequatur ducimus cum aliquam est minus provident placeat, nesciunt vitae. 
+            Et aperiam magni non quasi est asperiores nobis atque suscipit incidunt dignissimos! Harum et sunt ea.
+
+            When she reached the first hills of the Italic Mountains, she had a last view back on the skyline 
+            of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, 
+            the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.
+
+            Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.
+            Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+
+            A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+            It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+            `,
+            published: "Jun 10,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 5,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/posts/img5.png",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, qui ea assumenda illo quidem 
+            consequatur ducimus cum aliquam est minus provident placeat, nesciunt vitae. 
+            Et aperiam magni non quasi est asperiores nobis atque suscipit incidunt dignissimos! Harum et sunt ea.
+
+            When she reached the first hills of the Italic Mountains, she had a last view back on the skyline 
+            of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, 
+            the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.
+
+            Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.
+            Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+
+            A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+            It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+            `,
+            published: "Jun 14,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 6,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/posts/img2.png",
+            description: `
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt rerum inventore, harum accusantium magnam ducimus iusto sunt quisquam in, error laborum? Tempore possimus inventore quo explicabo exercitationem! Sint, quaerat.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, qui ea assumenda illo quidem 
+            consequatur ducimus cum aliquam est minus provident placeat, nesciunt vitae. 
+            Et aperiam magni non quasi est asperiores nobis atque suscipit incidunt dignissimos! Harum et sunt ea.
+
+            When she reached the first hills of the Italic Mountains, she had a last view back on the skyline 
+            of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, 
+            the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.
+
+            Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.
+            Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+
+            A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+            It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+            `,
+            published: "Jun 5,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+    ],
+    Popular : [
+        {
+            id: 1,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/img1.jpg",
+            description: `
+            "Unleash Your Inner Shopaholic: Discover the Latest Fashion and Home Decor Trends in Our Online Store!
+            `,
+            published: "Jun 3,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 2,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/img2.jpg",
+            description: `
+            "Unleash Your Inner Shopaholic: Discover the Latest Fashion and Home Decor Trends in Our Online Store!
+            `,
+            published: "Jun 5,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 3,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/img3.jpg",
+            description: `
+            "Unleash Your Inner Shopaholic: Discover the Latest Fashion and Home Decor Trends in Our Online Store!
+            `,
+            published: "Jun 7,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 4,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/img4.png",
+            description: `
+            "Unleash Your Inner Shopaholic: Discover the Latest Fashion and Home Decor Trends in Our Online Store!
+            `,
+            published: "Jan 10,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        },
+        {
+            id: 5,
+            title: "Shop 'til You Drop: Our Latest Collection for Fashion and Home Decor!",
+            subtitle: "Experience Shopping Nirvana: Explore Our Online Store for Trendy Fashion and Home Decor!.",
+            category: "Fashion, Decor",
+            img : "/images/articles/img5.png",
+            description: `
+            "Unleash Your Inner Shopaholic: Discover the Latest Fashion and Home Decor Trends in Our Online Store!
+            `,
+            published: "May 24,  2023",
+            author: {
+                name: "John Deo",
+                img: "/images/author/author1.jpg",
+                designation : "CEO and Founder"
+            }
+        }
+    ]
+}
+
